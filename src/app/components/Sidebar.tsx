5c030755b692ede13f45cf07ab37a290d71e96c8
Vito Huang <vito.formosa@outlook.com>
2023-06-26 19:02:12 +0800

    feat: add crud of account page

---
+++ src/app/components/Sidebar.tsx
@@ -0,0 +1,21 @@
+import menu from '../../../public/menu.png'
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function Sidebar() {
+  return (
+    <div className='flex flex-col gap-y-[60px] items-center w-[90px] h-screen pt-[30px] fixed left-0 top-0 border-r border-[#D9D9D9]'>
+      <div>
+        <Image src={menu} alt='menu' className='cursor-pointer' />
+      </div>
+      <div className='flex flex-col gap-y-[35px] items-center'>
+        <Link href='#'>資產</Link>
+        <Link href='/dashboard/account'>帳戶</Link>
+        <Link href='#'>明細</Link>
+        <Link href='#'>分析</Link>
+        <Link href='#'>預算</Link>
+        <Link href='#'>個人資料</Link>
+      </div>
+    </div>
+  )
+}
